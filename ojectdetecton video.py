b5895734d270c8fb8219a13cd8d58e60a97d444a
Lokesh Reddy <30724873+lokeshreddy007@users.noreply.github.com>
2018-02-26 17:57:05 +0530

    READ.md
    This an an oblect detection on video using an Tensorflow and Opencv libraries in python

---
+++ ojectdetecton video.py
@@ -0,0 +1,113 @@
+import numpy as np
+import os
+import six.moves.urllib as urllib
+import sys
+import tarfile
+import tensorflow as tf
+import zipfile
+
+from collections import defaultdict
+from io import StringIO
+from matplotlib import pyplot as plt
+from PIL import Image
+
+
+sys.path.append("..")
+from object_detection.utils import ops as utils_ops
+
+if tf.__version__ < '1.4.0':
+  raise ImportError('Please upgrade your tensorflow installation to v1.4.* or later!')
+
+
+
+# This is needed to display the images.
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+from utils import label_map_util
+
+from utils import visualization_utils as vis_util
+
+PATH_TO_CKPT = './ssd_mobilenet_v1_coco_2017_11_17/frozen_inference_graph.pb'
+
+PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
+
+NUM_CLASSES = 90
+
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+    od_graph_def = tf.GraphDef()
+    with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+        serialized_graph = fid.read()
+        od_graph_def.ParseFromString(serialized_graph)
+        tf.import_graph_def(od_graph_def, name='')
+        
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
+
+
+
+import cv2
+VIDEO_NAME = 'Taylor.mp4'
+CWD_PATH = os.getcwd()
+filename="./output12.avi"
+codec=cv2.VideoWriter_fourcc('m','p','4','v')#fourcc stands for four character code
+framerate=30
+resolution=(640,480)
+VideoFileOutput=cv2.VideoWriter(filename,codec,framerate, resolution)
+PATH_TO_VIDEO = os.path.join(CWD_PATH,VIDEO_NAME)
+
+
+# Input tensor is the image
+image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+
+# Output tensors are the detection boxes, scores, and classes
+# Each box represents a part of the image where a particular object was detected
+detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+
+# Each score represents level of confidence for each of the objects.
+# The score is shown on the result image, together with the class label.
+detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+
+# Number of objects detected
+num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
+# Open video file
+video = cv2.VideoCapture(PATH_TO_VIDEO)
+
+while(video.isOpened()):
+
+    # Acquire frame and expand frame dimensions to have shape: [1, None, None, 3]
+    # i.e. a single-column array, where each item in the column has the pixel RGB value
+    ret, frame = video.read()
+    frame_expanded = np.expand_dims(frame, axis=0)
+    image_np_expanded = np.expand_dims(frame, axis=0)
+    # Perform the actual detection by running the model with the image as input
+    (boxes, scores, classes, num) = sess.run(
+        [detection_boxes, detection_scores, detection_classes, num_detections],
+        feed_dict={image_tensor: frame_expanded})
+#     image_np_expanded = np.expand_dims(frame, axis=0)
+    # Draw the results of the detection (aka 'visulaize the results')
+    vis_util.visualize_boxes_and_labels_on_image_array(
+        frame,
+        np.squeeze(boxes),
+        np.squeeze(classes).astype(np.int32),
+        np.squeeze(scores),
+        category_index,
+        use_normalized_coordinates=True,
+        line_thickness=8,
+        min_score_thresh=0.80)
+    # All the results have been drawn on the frame, so it's time to display it.
+    cv2.imshow('Object detector', frame)
+    VideoFileOutput.write(frame)
+    # Press 'q' to quit
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+# Clean up
+video.release()
+cv2.destroyAllWindows()
+
